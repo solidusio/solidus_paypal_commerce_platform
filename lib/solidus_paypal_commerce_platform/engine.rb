2100b4b63058e64ba8c627d6b54a8f29c0468427
Sean <seand7565@gmail.com>
2020-05-11 14:37:55 -0500

    Add setup wizard for paypal commerce platform
    Adds the ability to create and add setup wizards for various things
    Adds a setup wizard (currently missing a logo and return_url) for PCP
    Adds return code for when the user returns from PayPal with sensitive info
    Still have to build a route to handle that info on return.

--- lib/solidus_paypal_commerce_platform/engine.rb
+++ lib/solidus_paypal_commerce_platform/engine.rb
@@ -14,6 +14,12 @@ module SolidusPaypalCommercePlatform
     initializer "register_solidus_paypal_commerce_platform_gateway", after: "spree.register.payment_methods" do |app|
       app.config.spree.payment_methods << SolidusPaypalCommercePlatform::Gateway
     end
+    
+    initializer "register_solidus_paypal_commerce_platform_wizard", after: "spree.register.payment_methods" do |app|
+      # Adding the class set below - if this is ported to core, we can remove this line.
+      Spree::Core::Environment.add_class_set("payment_setup_wizards")
+      app.config.spree.payment_setup_wizards << "SolidusPaypalCommercePlatform::Wizard"
+    end
 
     # use rspec for tests
     config.generators do |g|
