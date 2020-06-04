a5810ed4a913cf6b8a2108c87355848c2114b6b4
Elia Schito <elia@schito.me>
2020-06-04 16:50:39 +0200

    Lock the PayPal env to the payment test_mode value
    Before this it was always using the sandbox env, even in production and
    with the method's test_mode as false.

--- app/controllers/solidus_paypal_commerce_platform/wizard_controller.rb
+++ app/controllers/solidus_paypal_commerce_platform/wizard_controller.rb
@@ -19,19 +19,24 @@ module SolidusPaypalCommercePlatform
 
     private
 
+    def use_sandbox?
+      !Rails.env.production?
+    end
+
     def payment_method_params
       {
         name: "PayPal Commerce Platform",
         type: SolidusPaypalCommercePlatform::Gateway,
         preferred_client_id: api_credentials.client_id,
         preferred_client_secret: api_credentials.client_secret,
-        preferred_test_mode: !Rails.env.production?
+        preferred_test_mode: use_sandbox?
       }
     end
 
     def api_credentials
       @api_credentials ||= begin
-        paypal_env = PayPal::SandboxEnvironment.new(credentials[:sharedId])
+        paypal_env_class = use_sandbox? ? PayPal::SandboxEnvironment : PayPal::LiveEnvironment
+        paypal_env = paypal_env_class.new(params.fetch(:sharedId), nil)
         paypal_client = SolidusPaypalCommercePlatform::Requests.new(paypal_env)
 
         paypal_client.trade_tokens(params)
