878392f5a8afbb043b492767892b8a0eff73926b
Elia Schito <elia@schito.me>
2020-06-08 19:57:54 +0200

    Make the wizard setup override work with the blank slate
    The previous selector was only working if a payment method already existed.

---
+++ app/overrides/spree/admin/payment_methods/index/insert_setup_wizards.rb
@@ -0,0 +1,15 @@
+Deface::Override.new(
+  name: "payment_methods/index/insert_setup_wizards#list",
+  virtual_path: "spree/admin/payment_methods/index",
+  original: "bf5a94b3b0967d88b88f72bc26131897ff85a1d0",
+  insert_after: "#listing_payment_methods",
+  partial: 'solidus_paypal_commerce_platform/admin/payment_methods/setup_wizards'
+)
+
+Deface::Override.new(
+  name: "payment_methods/index/insert_setup_wizards#empty",
+  virtual_path: "spree/admin/payment_methods/index",
+  original: "318d918b15df51842a5d98a6230bea96ffa05142",
+  insert_after: ".no-objects-found",
+  partial: 'solidus_paypal_commerce_platform/admin/payment_methods/setup_wizards'
+)
