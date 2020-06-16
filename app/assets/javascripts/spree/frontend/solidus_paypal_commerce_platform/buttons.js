1916507a66091ba75a0fd89b204ed73741178c8b
Sean <seand7565@gmail.com>
2020-06-16 15:38:39 -0500

    Add onShippingChange callback to PayPal buttons
    Hits our API to get simulated shipping rate/tax changes and
    sends the simulated order to PayPal. This allows the changes
    made to be displayed to the user - charging them for the difference
    in tax and shipping.

--- app/assets/javascripts/spree/frontend/solidus_paypal_commerce_platform/buttons.js
+++ app/assets/javascripts/spree/frontend/solidus_paypal_commerce_platform/buttons.js
@@ -17,6 +17,21 @@ SolidusPaypalCommercePlatform.renderButton = function(payment_method_id, style) 
     onApprove: function (data, actions) {
       $("#payments_source_paypal_order_id").val(data.orderID)
       $("#checkout_form_payment").submit()
+    },
+    onShippingChange: function(data, actions) {
+      Spree.ajax({
+        url: '/solidus_paypal_commerce_platform/shipping_rates',
+        method: 'GET',
+        data: {
+          order_id: Spree.current_order_id,
+          order_token: Spree.current_order_token,
+          address: data.shipping_address
+        }
+      }).then(function(res) {
+        return actions.order.patch([
+          res
+        ]);
+      });
     }
   }).render('#paypal-button-container')
 }
