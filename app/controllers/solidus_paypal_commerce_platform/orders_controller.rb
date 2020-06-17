5bdd17432bb76a862ccfc71858def8a2cf199c41
Sean <seand7565@gmail.com>
2020-06-17 10:56:38 -0500

    Add API endpoint for updating an orders address from a PayPal address
    If the address change fails, it returns the reason why so we can display
    it to the customer.

--- app/controllers/solidus_paypal_commerce_platform/orders_controller.rb
+++ app/controllers/solidus_paypal_commerce_platform/orders_controller.rb
@@ -1,12 +1,22 @@
 module SolidusPaypalCommercePlatform
   class OrdersController < ::Spree::Api::BaseController
     before_action :load_order
-    before_action :load_payment_method
+    before_action :load_payment_method, only: :create
     skip_before_action :authenticate_user
 
     def create
       authorize! :update, @order, order_token
       render json: @payment_method.gateway.create_order(@order, @payment_method.auto_capture), status: :ok
+    end
+
+    def update_address
+      authorize! :update, @order, order_token
+      address = SolidusPaypalCommercePlatform::PaypalAddress.new(@order).update_address(params[:address])
+      if address.errors.any?
+        render json: address.errors.full_messages, status: :unprocessable_entity
+      else
+        render json: {}, status: :ok
+      end
     end
 
     private
