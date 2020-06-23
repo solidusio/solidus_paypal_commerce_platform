e0542d6c91f6a300309aa44d395ff60adba66402
Elia Schito <elia@schito.me>
2020-06-23 19:07:32 +0200

    Add some basic specs for the Wizard

--- spec/features/backend/new_payment_method_spec.rb
+++ spec/features/backend/new_payment_method_spec.rb
@@ -7,4 +7,19 @@ RSpec.describe "creating a new payment" do
     visit "/admin/payment_methods/new"
     expect(page).to have_select('payment_method_type', :options => ["Paypal Commerce Platform", "Bogus Credit Card Payments", "Check Payments", "Simple Bogus Credit Card Payments", "Store Credit Payments"])
   end
+
+  scenario "should display the onboarding button", :js do
+    visit "/admin/payment_methods"
+
+    main_window = current_window
+
+    within ".setup-wizard" do
+      paypal_button = page.find("button")
+      expect(paypal_button.text).to eq("Setup PayPal Commerce Platform")
+      # TODO: make this more complete
+      # paypal_window = window_opened_by do
+      #   paypal_button.click
+      # end
+    end
+  end
 end
---
+++ spec/requests/solidus_paypal_commerce_platform/wizard_controller_spec.rb
@@ -0,0 +1,40 @@
+require 'spec_helper'
+
+RSpec.describe SolidusPaypalCommercePlatform::WizardController, type: :request do
+  stub_authorization!
+
+  describe "POST /solidus_paypal_commerce_platform/wizard" do
+    let(:params) { {
+      authCode: "PFc4d2vp4DVfHqcnEHbGAA12C_H29U39NM_vmQrZBPzdLcxA12Br-GzjbliqXGu3AG6Gfwo5G9GTal6REkcKGMREc9fPsR_wv",
+      sharedId: "awj45zMAy1XonxWfgIhjjGHZGAPStkZFzXp4bfe1QmNWA-9DL6HkpklD0skHig4vVF7zVZD8Uwy5Qop4",
+      nonce: "0fb0e3e75e3deb58de8fb4e8dc27eb25ab2cbcabdb84"
+    } }
+
+    it "creates a new payment method from data received from PayPal" do
+      expect_any_instance_of(SolidusPaypalCommercePlatform::Client).to receive(:execute) do |client, request|
+        case request
+        when SolidusPaypalCommercePlatform::AccessTokenAuthorizationRequest
+          OpenStruct.new(result: OpenStruct.new(access_token: "ACCESS-TOKEN"))
+        when SolidusPaypalCommercePlatform::FetchMerchantCredentialsRequest
+          expect(request.headers.fetch("Authorization")).to eq("Bearer ACCESS-TOKEN")
+          OpenStruct.new(result: OpenStruct.new(
+            client_id: "CLIENT-ID",
+            client_secret: "CLIENT-SECRET",
+          ))
+        else
+          raise "unexpected request: #{request}"
+        end
+      end.twice
+
+      expect {
+        post solidus_paypal_commerce_platform.wizard_index_path, params: params
+      }.to change(SolidusPaypalCommercePlatform::PaymentMethod, :count).from(0).to(1)
+
+      payment_method = SolidusPaypalCommercePlatform::PaymentMethod.last
+
+      expect(payment_method.preferred_client_id).to eq("CLIENT-ID")
+      expect(payment_method.preferred_client_secret).to eq("CLIENT-SECRET")
+      expect(response).to have_http_status(201)
+    end
+  end
+end
