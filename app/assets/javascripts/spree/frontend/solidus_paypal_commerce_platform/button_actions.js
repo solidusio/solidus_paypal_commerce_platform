f233b8d9e5b23cf1e68fd9cb17e4b60bed5b5b64
Sean <seand7565@gmail.com>
2020-06-24 07:25:50 -0500

    Add ability to create a PayPal button on the cart page
    Adds a few new button actions revolving around checking out from the cart page,
    and adds a new PayPal Button method that generates a button with these new actions

--- app/assets/javascripts/spree/frontend/solidus_paypal_commerce_platform/button_actions.js
+++ app/assets/javascripts/spree/frontend/solidus_paypal_commerce_platform/button_actions.js
@@ -7,27 +7,14 @@ SolidusPaypalCommercePlatform.postOrder = function(payment_method_id) {
       order_id: Spree.current_order_id,
       order_token: Spree.current_order_token
     }
-  }).then(function(res) {
-    return res.table.id;
+  }).then(function(response) {
+    return response.table.id;
   })
 }
 
 SolidusPaypalCommercePlatform.approveOrder = function(data, actions) {
-  actions.order.get().then(function(res){
-    var updated_address = res.purchase_units[0].shipping.address
-    Spree.ajax({
-      url: '/solidus_paypal_commerce_platform/update_address',
-      method: 'POST',
-      data: {
-        address: updated_address,
-        order_id: Spree.current_order_id,
-        order_token: Spree.current_order_token
-      },
-      error: function(response) {
-        message = response.responseJSON;
-        alert('There were some problems with your payment address - ' + message);
-      }
-    }).then(function() {
+  actions.order.get().then(function(response){
+    SolidusPaypalCommercePlatform.updateAddress(response).then(function() {
       $("#payments_source_paypal_order_id").val(data.orderID)
       $("#checkout_form_payment").submit()
     })
@@ -48,7 +35,73 @@ SolidusPaypalCommercePlatform.shippingChange = function(data, actions) {
       console.log('There were some problems with your payment address - ' + message);
       actions.reject()
     }
-  }).then(function(res) {
-    actions.order.patch([res]);
+  }).then(function(response) {
+    actions.order.patch([response]);
   });
+}
+
+SolidusPaypalCommercePlatform.finalizeOrder = function(payment_method_id, data, actions) {
+  actions.order.get().then(function(response){
+    SolidusPaypalCommercePlatform.updateAddress(response).then(function() {
+      var paypal_amount = response.purchase_units[0].amount.value
+      SolidusPaypalCommercePlatform.addPayment(paypal_amount, payment_method_id, data).then(function() {
+        SolidusPaypalCommercePlatform.advanceOrder().then(function(response) {
+          window.location.href = SolidusPaypalCommercePlatform.checkout_url
+        })
+      })
+    })
+  })
+}
+
+SolidusPaypalCommercePlatform.advanceOrder = function() {
+  return Spree.ajax({
+    url: '/api/checkouts/' + Spree.current_order_id + '/advance',
+    method: 'PUT',
+    data: {
+      order_token: Spree.current_order_token
+    },
+    error: function(response) {
+      alert('There were some problems with your order');
+    }
+  })
+}
+
+SolidusPaypalCommercePlatform.addPayment = function(paypal_amount, payment_method_id, data) {
+  return Spree.ajax({
+    url: '/api/checkouts/' + Spree.current_order_id + '/payments',
+    method: 'POST',
+    data: {
+      order_token: Spree.current_order_token,
+      payment: {
+        amount: paypal_amount,
+        payment_method_id: payment_method_id,
+        source_attributes: {
+          paypal_order_id: data.orderID
+        }
+      }
+    },
+    error: function(response) {
+      alert('There were some problems with your payment');
+    }
+  })
+}
+
+SolidusPaypalCommercePlatform.updateAddress = function(response) { 
+  var updated_address = response.purchase_units[0].shipping.address
+  return Spree.ajax({
+    url: '/solidus_paypal_commerce_platform/update_address',
+    method: 'POST',
+    data: {
+      address: {
+        updated_address: updated_address,
+        recipient: response.payer
+      },
+      order_id: Spree.current_order_id,
+      order_token: Spree.current_order_token
+    },
+    error: function(response) {
+      message = response.responseJSON;
+      alert('There were some problems with your payment address - ' + message);
+    }
+  })
 }
--- app/assets/javascripts/spree/frontend/solidus_paypal_commerce_platform/buttons.js
+++ app/assets/javascripts/spree/frontend/solidus_paypal_commerce_platform/buttons.js
@@ -6,3 +6,12 @@ SolidusPaypalCommercePlatform.renderButton = function(payment_method_id, style) 
     onShippingChange: SolidusPaypalCommercePlatform.shippingChange
   }).render('#paypal-button-container')
 }
+
+SolidusPaypalCommercePlatform.renderCartButton = function(payment_method_id, style) {
+  paypal.Buttons({
+    style: style,
+    createOrder: SolidusPaypalCommercePlatform.postOrder.bind(null, payment_method_id),
+    onApprove: SolidusPaypalCommercePlatform.finalizeOrder.bind(null, payment_method_id),
+    onShippingChange: SolidusPaypalCommercePlatform.shippingChange
+  }).render('#paypal-button-container')
+}
