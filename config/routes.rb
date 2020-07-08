e1b20a78f0cc9d2f8aa2f1e7b3d1c9716cfef01c
Sean <seand7565@gmail.com>
2020-07-08 15:39:02 -0500

    Add API endpoint to verify amount to be charged
    We need to be able to verify that the amount we receive from PayPal
    equals the outstanding balance of the order. So, this API endpoint
    allows us to check the paypal_balance with the orders outstanding_total.

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,5 @@ SolidusPaypalCommercePlatform::Engine.routes.draw do
   resources :payments, only: [:create]
   get :shipping_rates, to: "shipping_rates#simulate_shipping_rates"
   post :update_address, to: "orders#update_address"
+  get :verify_total, to: "orders#verify_total"
 end
