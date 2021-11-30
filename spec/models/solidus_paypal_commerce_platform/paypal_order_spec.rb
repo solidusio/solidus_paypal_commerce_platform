bcb2d817ecc04a7dc4acf36c22821360adb7d19d
Daniele Palombo <ksemuldie@gmail.com>
2021-11-30 13:08:13 +0100

    Truncate the product name to 127 characters

--- spec/models/solidus_paypal_commerce_platform/paypal_order_spec.rb
+++ spec/models/solidus_paypal_commerce_platform/paypal_order_spec.rb
@@ -21,6 +21,23 @@ RSpec.describe SolidusPaypalCommercePlatform::PaypalOrder, type: :model do
       )
     end
 
+    context 'when the item name is greater than 127 characters' do
+      let(:variant) { create(:variant, product: create(:product, name: 'a' * 128)) }
+      let(:order) do
+        create(:order_ready_to_complete, line_items_attributes: [{ variant: variant, quantity: 1, price: 10 }])
+      end
+
+      it 'returns the truncated name' do
+        expect(to_json).to match hash_including(
+          purchase_units: array_including(
+            hash_including(
+              items: array_including(hash_including(name: "#{'a' * 124}..."))
+            )
+          )
+        )
+      end
+    end
+
     context 'when checkout_steps does not include "delivery"' do
       let(:old_checkout_flow) { Spree::Order.checkout_flow }
 
