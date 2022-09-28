4fd6bc031177dc487d58eb0287ffd81a0ec10dcb
Elia Schito <elia@schito.me>
2022-09-28 16:38:13 +0200

    Use the standard route helper to add the engine mount line

--- lib/generators/solidus_paypal_commerce_platform/install/install_generator.rb
+++ lib/generators/solidus_paypal_commerce_platform/install/install_generator.rb
@@ -27,9 +27,7 @@ module SolidusPaypalCommercePlatform
       end
 
       def mount_engine
-        insert_into_file File.join('config', 'routes.rb'), after: "Rails.application.routes.draw do\n" do
-          "mount SolidusPaypalCommercePlatform::Engine, at: '/solidus_paypal_commerce_platform'\n"
-        end
+        route "mount SolidusPaypalCommercePlatform::Engine, at: '/solidus_paypal_commerce_platform'"
       end
 
       def run_migrations
