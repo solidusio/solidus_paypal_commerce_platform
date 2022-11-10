19fa6766b8219d77b01a402d852c71c489e11dcc
Elia Schito <elia@schito.me>
2022-11-10 15:09:50 +0100

    Reorganize the installer into sections
    One for each solidus dependency, that eventually can be skipped in
    case the dependency is not part of the bundle.

--- lib/generators/solidus_paypal_commerce_platform/install/install_generator.rb
+++ lib/generators/solidus_paypal_commerce_platform/install/install_generator.rb
@@ -10,32 +10,62 @@ module SolidusPaypalCommercePlatform
 
       source_root File.expand_path('templates', __dir__)
 
-      def copy_initializer
+      def install_solidus_core_support
         template 'initializer.rb', 'config/initializers/solidus_paypal_commerce_platform.rb'
-      end
-
-      def add_javascripts
-        append_file 'vendor/assets/javascripts/spree/frontend/all.js', "//= require spree/frontend/solidus_paypal_commerce_platform\n" # rubocop:disable Layout/LineLength
-        append_file 'vendor/assets/javascripts/spree/backend/all.js', "//= require spree/backend/solidus_paypal_commerce_platform\n" # rubocop:disable Layout/LineLength
-      end
-
-      def add_stylesheets
-        inject_into_file 'vendor/assets/stylesheets/spree/frontend/all.css', " *= require spree/frontend/solidus_paypal_commerce_platform\n", before: %r{\*/}, verbose: true # rubocop:disable Layout/LineLength
-        inject_into_file 'vendor/assets/stylesheets/spree/backend/all.css', " *= require spree/backend/solidus_paypal_commerce_platform\n", before: %r{\*/}, verbose: true # rubocop:disable Layout/LineLength
-      end
-
-      def add_migrations
-        run 'bin/rails railties:install:migrations FROM=solidus_paypal_commerce_platform'
-      end
-
-      def mount_engine
+        rake 'railties:install:migrations FROM=solidus_paypal_commerce_platform'
         route "mount SolidusPaypalCommercePlatform::Engine, at: '/solidus_paypal_commerce_platform'"
+      end
+
+      def install_solidus_backend_support
+        support_code_for('solidus_backend') do
+          append_file(
+            'vendor/assets/javascripts/spree/backend/all.js',
+            "//= require spree/backend/solidus_paypal_commerce_platform\n"
+          )
+          inject_into_file(
+            'vendor/assets/stylesheets/spree/backend/all.css',
+            " *= require spree/backend/solidus_paypal_commerce_platform\n",
+            before: %r{\*/},
+            verbose: true,
+          )
+        end
+      end
+
+      def install_solidus_frontend_support
+        support_code_for('solidus_frontend') do
+          append_file(
+            'vendor/assets/javascripts/spree/frontend/all.js',
+            "//= require spree/frontend/solidus_paypal_commerce_platform\n",
+          )
+          inject_into_file(
+            'vendor/assets/stylesheets/spree/frontend/all.css',
+            " *= require spree/frontend/solidus_paypal_commerce_platform\n",
+            before: %r{\*/},
+            verbose: true,
+          )
+          directory engine.root.join("lib/views/frontend"), 'app/views/'
+        end
       end
 
       def run_migrations
         return rake 'db:migrate' if options[:auto_run_migrations] && !options[:skip_migrations]
 
         say_status :skip, 'db:migrate'
+      end
+
+      private
+
+      def support_code_for(gem_name, run_if: Bundler.locked_gems.specs.map(&:name).include?(gem_name), &block)
+        if run_if
+          say_status :install, "[#{engine.engine_name}] #{gem_name} code", :blue
+          shell.indent(&block)
+        else
+          say_status :skip, "[#{engine.engine_name}] #{gem_name} code", :blue
+        end
+      end
+
+      def engine
+        SolidusPaypalCommercePlatform::Engine
       end
     end
   end
