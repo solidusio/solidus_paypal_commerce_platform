fcdd5947151ca3432755a3df80890c03799646d2
Martin Meyerhoff <mamhoff@gmail.com>
2023-01-02 14:17:15 +0100

    Do not break custom pricing options classes
    Prior to this commit, installing this gem would break custom pricing
    options classes in host apps: By overriding the getter on
    `Spree::Config`, it was very difficult to use a custom pricing options
    class. By instead decorating the base class, we make sure that even if
    host app developers have used their own pricing options class, that
    pricing options class gets the right `cache_key` method (provided they
    themselves overrode `cache_key` using `super`).

---
+++ app/decorators/models/solidus_paypal_commerce_platform/spree/variant/pricing_options_decorator.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+module SolidusPaypalCommercePlatform
+  module Spree
+    module Variant
+      module PricingOptionsDecorator
+        def cache_key
+          SolidusPaypalCommercePlatform::PaymentMethod.
+            active.
+            map(&:cache_key_with_version).
+            sort + [super]
+        end
+
+        ::Spree::Variant::PricingOptions.prepend self
+      end
+    end
+  end
+end
--- app/models/solidus_paypal_commerce_platform/pricing_options.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-module SolidusPaypalCommercePlatform
-  class PricingOptions < ::Spree::Variant::PricingOptions
-    def cache_key
-      SolidusPaypalCommercePlatform::PaymentMethod.active.map(&:cache_key_with_version).sort + [super]
-    end
-  end
-end
