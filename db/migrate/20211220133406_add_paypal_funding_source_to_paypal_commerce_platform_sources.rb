de2a4aca78b208e1c22012f0cf3c2bcec3a465d4
Thomas von Deyen <thomas@vondeyen.com>
2023-03-29 15:41:45 +0200

    Fix migration version
    We still support Rails 5.2 with this gem, because we allow Solidus
    3.0, which in return still allows Rails 5.2. Installing this extension
    during a dummy app install in another extension fails, if the
    dummy app still is a Rails 6.0 app.

--- db/migrate/20211220133406_add_paypal_funding_source_to_paypal_commerce_platform_sources.rb
+++ db/migrate/20211220133406_add_paypal_funding_source_to_paypal_commerce_platform_sources.rb
@@ -1,4 +1,4 @@
-class AddPaypalFundingSourceToPaypalCommercePlatformSources < ActiveRecord::Migration[6.1]
+class AddPaypalFundingSourceToPaypalCommercePlatformSources < ActiveRecord::Migration[5.2]
   def change
     add_column :paypal_commerce_platform_sources, :paypal_funding_source, :integer
   end
