2ef4b326ad6fec911543b5c5ef59687745eab70f
Martin Meyerhoff <mamhoff@gmail.com>
2025-01-22 13:48:59 +0100

    Use Flickwerk-style patches
    This will only turn green when solidus_support with flickwerk support is
    released.

--- app/patches/models/solidus_paypal_commerce_platform/spree/address_patch.rb
+++ app/patches/models/solidus_paypal_commerce_platform/spree/address_patch.rb
@@ -2,7 +2,7 @@
 
 module SolidusPaypalCommercePlatform
   module Spree
-    module AddressDecorator
+    module AddressPatch
       # PayPal doesn't use the phone number, so in cases where the user checks out via
       # PayPal, this field will be unpopulated. If you want to require phone numbers,
       # you'll need to turn off cart & product page displays on the payment method edit
