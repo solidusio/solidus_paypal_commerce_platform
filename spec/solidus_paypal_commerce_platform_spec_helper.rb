249b4be5b162319a08ef2cee0e3517387efb3db2
Alistair Norman <alistair@super.gd>
2026-06-17 13:29:34 -0700

    Allow for starter_frontend rename
    The changes here won't necessarily be merged at the exact same time as
    the other changes to solidus_storefront/starter_frontend so I made this
    all work with either name.

--- spec/solidus_paypal_commerce_platform_spec_helper.rb
+++ spec/solidus_paypal_commerce_platform_spec_helper.rb
@@ -1,5 +1,9 @@
 # frozen_string_literal: true
 
-require 'solidus_starter_frontend_spec_helper'
+begin
+  require 'solidus_storefront_spec_helper'
+rescue LoadError
+  require 'solidus_starter_frontend_spec_helper'
+end
 
 Dir["#{__dir__}/support/solidus_paypal_commerce_platform/**/*.rb"].sort.each { |f| require f }
